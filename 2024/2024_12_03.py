9706cad44abb3b0131dad5ed9f7428c62b529369
Katka Halasova <katarina.kaderova@gmail.com>
2025-05-05 15:21:10 +0200

    part two of day 3 2024

--- 2024/2024_12_03.py
+++ 2024/2024_12_03.py
@@ -9,27 +9,49 @@ corupted_memory = ''
 with open("2024/additional_files/corrupted_memory.txt", "r") as f:
     for line in f:
         corupted_memory = corupted_memory + line
-# print(corupted_memory)
 
 # What do you get if you add up all of the results of the multiplications?
+
+def scanning(corupted_memory):
+    return re.findall(r"(?:mul\()\d{1,3}[,]\d{1,3}(?:\))", corupted_memory)
 
 def sum_multiplications(list_of_pairs):
     sum = 0
     for pair in list_of_pairs:
         # removing braces and changing data type from string to integer
         a,b = [int(e) for e in pair.lstrip('mul(').rstrip(')').split(',')]
-        # print(a)
-        # print(b)
         sum += a*b
-        # print('sum')
-        # print(sum)
     return sum
 # scanning uncorrupted multiplications
 scanned = []
-scanned = re.findall(r"(?:mul\()\d{1,3}[,]\d{1,3}(?:\))",corupted_memory)
+scanned = scanning(corupted_memory)
 adding_multiplications = sum_multiplications(scanned)
-#print(scanned)
 print(adding_multiplications)
 
 # Part 2
+# example
+# corupted_memory = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'
 
+# find do() and don't() in string, split to list and take only parts starting with do()
+do_positions = [i for i in range(len(corupted_memory)) if corupted_memory.startswith('do()', i)]
+dont_positions = [i for i in range(len(corupted_memory)) if corupted_memory.startswith('don\'t()', i)]
+do_positions.append(0) # by default we consider enabled from start    
+do_positions.sort()
+
+# we split string corupted_memory and take all substrings starting with 'do()' 
+add_mul = 0
+for i, index_of_start in enumerate(do_positions):
+    # list of possible end-indices for substring (endind with "don't")
+    list_of_end_index = [index for index in dont_positions if index_of_start <= index]
+    # or substring can end by next 'do()'
+    if i < (len(do_positions)-1): 
+        list_of_end_index.append(do_positions[i+1])
+    else:
+        # if there is no more 'do()', then consider end of original string
+        list_of_end_index.append(len(corupted_memory)-1)
+    # final end-index of substring
+    index_of_end = min(list_of_end_index)
+    # and do what we 
+    add_mul += sum_multiplications(scanning(corupted_memory[index_of_start:index_of_end]))
+print(scanned)
+print(add_mul)
